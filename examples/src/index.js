d519db60068cc3b24948a586fd56d177a629b145
SodaPop Admin <admin@SodaPopSystems.com>
2022-01-13 13:01:46 -0800

    Allow textfield props to pass through to the start / end inputs

--- examples/src/index.js
+++ examples/src/index.js
@@ -13,7 +13,7 @@ const App = () => {
             },
             input: {}
         },
-        to: {
+        separator: {
             container: {
                 float: 'left',
                 marginTop: 15,
--- src/material-ui-ranger.pres.js
+++ src/material-ui-ranger.pres.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import {Grid, TextField, Typography} from '@mui/material';
+import React, {useState} from 'react';
+import {TextField, Typography, Grid} from '@material-ui/core';
 
 const MaterialUIRanger = props => {
-    const {styles, startPlaceholder, endPlaceholder, onStartChanged, onEndChanged, numbersOnly, separatorValue, separatorValueVariant} = props;
+    const {styles, startInputProps, startPlaceholder, endInputProps, endPlaceholder, onStartChanged, onEndChanged, numbersOnly, separatorValue, separatorValueVariant} = props;
 
     let inputType = () => {
         let type = "text"
@@ -16,9 +16,13 @@ const MaterialUIRanger = props => {
         <Grid container>
             <Grid item lg={12} md={12} sm={12}>
                 <div style={styles.controlsContainer}>
-                    <div style={styles.start.container}><TextField style={styles.start.input} type={inputType()} onChange={onStartChanged} placeholder={startPlaceholder}/></div>
-                    <div style={styles.to.container}><Typography style={styles.to.typography} variant={separatorValueVariant}>{separatorValue}</Typography></div>
-                    <div style={styles.end.container}><TextField style={styles.end.input} type={inputType()} onChange={onEndChanged} placeholder={endPlaceholder}/></div>
+                    <div style={styles.start.container}>
+                        <TextField {...startInputProps} style={styles.start.input} type={inputType()} onChange={onStartChanged} placeholder={startPlaceholder}/>
+                    </div>
+                    <div style={styles.separator.container}><Typography style={styles.separator.typography} variant={separatorValueVariant}>{separatorValue}</Typography></div>
+                    <div style={styles.end.container}>
+                        <TextField {...endInputProps} style={styles.end.input} type={inputType()} onChange={onEndChanged} placeholder={endPlaceholder}/>
+                    </div>
                 </div>
             </Grid>
         </Grid>
